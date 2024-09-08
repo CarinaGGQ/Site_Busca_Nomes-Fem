c40efc5a31100ee88f2f09682acf50503f6ee7fa
CarinaGGQ <140679743+CarinaGGQ@users.noreply.github.com>
2024-09-08 00:16:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,339 @@
+let dados = [
+  {
+    titulo: "Carina",
+    descricao:
+      "Carina é um nome feminino de origem italiana, que significa querida ou amada. Este nome é uma variação italiana de 'Cara', que também é um termo de carinho em italiano, evocando sentimentos de amor e ternura. Pessoas chamadas Carina geralmente são apreciadas por sua simpatia e natureza afetuosa.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/carina/",
+    tags: "carina Carina CARINA",
+  },
+  {
+    titulo: "Manuela",
+    descricao:
+      "Manuela é um nome de origem espanhola e italiana que deriva do nome masculino Manuel. Esse nome tem raízes hebraicas e significa 'Deus está conosco'. É um nome bastante comum em países de língua espanhola e portuguesa, além de ser apreciado em várias partes do mundo.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/manuela/",
+    tags: "manuela Manuela MANUELA",
+  },
+  {
+    titulo: "Luna",
+    descricao:
+      "O nome Luna tem origem latina e está relacionado à palavra 'lunaris', que significa lua. É um nome que remete à beleza e à serenidade do astro que ilumina as noites. É um nome de grande significado e beleza, trazendo consigo uma conexão com a natureza e o céu noturno.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/luna/",
+    tags: "luna Luna LUNA",
+  },
+
+  {
+    titulo: "Mariana",
+    descricao:
+      "Mariana é um nome feminino de origem latina que significa 'do mar'. É um nome bastante popular em países de língua portuguesa e espanhola. Mariana é frequentemente associado a uma pessoa gentil e sensível, refletindo um caráter suave e acolhedor.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/mariana/",
+    tags: "mariana Mariana MARIANA",
+  },
+  {
+    titulo: "Ana",
+    descricao:
+      "Ana é um nome feminino de origem hebraica que significa 'cheia de graça'. É um nome clássico e atemporal, muito popular em diversas culturas. Ana é frequentemente associada a características de simplicidade, pureza e elegância.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/ana/",
+    tags: "ana Ana ANA",
+  },
+  {
+    titulo: "Luisa",
+    descricao:
+      "Luisa é um nome feminino de origem germânica que significa 'lutadora gloriosa'. É um nome tradicional em vários países, associado a uma personalidade forte e determinada. Luisa é frequentemente vista como uma pessoa carinhosa e confiável.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/luisa/",
+    tags: "luisa Luisa LUISA",
+  },
+  {
+    titulo: "Giovana",
+    descricao:
+      "Giovana é um nome feminino de origem italiana que é uma variação de Giovanna, que significa 'Deus é gracioso'. É um nome popular em países de língua italiana e portuguesa, e é frequentemente associado a pessoas gentis e elegantes.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/giovana/",
+    tags: "giovana Giovana GIOVANA",
+  },
+  {
+    titulo: "Laura",
+    descricao:
+      "Laura é um nome feminino de origem latina que significa 'loureiro'. É um nome associado à vitória e à fama, e tem uma longa história de uso em diferentes culturas. Laura é frequentemente vista como uma pessoa sofisticada e criativa.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/laura/",
+    tags: "laura Laura LAURA",
+  },
+  {
+    titulo: "Letícia",
+    descricao:
+      "Letícia é um nome feminino de origem latina que significa 'alegria' ou 'felicidade'. É um nome que transmite uma sensação de leveza e otimismo, frequentemente associado a uma personalidade positiva e encantadora.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/leticia/",
+    tags: "leticia Leticia LETICIA Letícia LETÍCIA",
+  },
+  {
+    titulo: "Valéria",
+    descricao:
+      "Valéria é um nome feminino de origem latina que significa 'forte' ou 'saudável'. É um nome que transmite uma sensação de robustez e resistência, frequentemente associado a pessoas determinadas e corajosas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/valeria/",
+    tags: "valeria Valéria VALÉRIA",
+  },
+  {
+    titulo: "Milena",
+    descricao:
+      "Milena é um nome feminino de origem eslava que significa 'amorosa' ou 'carinhosa'. É um nome suave e elegante, frequentemente associado a uma personalidade afetuosa e encantadora.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/milena/",
+    tags: "milena Milena MILENA",
+  },
+  {
+    titulo: "Maísa",
+    descricao:
+      "Maísa é um nome feminino de origem indígena que pode significar 'milho'. É um nome com uma conotação natural e orgânica, frequentemente associado a uma personalidade vibrante e cheia de vida.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/maisa/",
+    tags: "maisa Maísa MAÍSA",
+  },
+  {
+    titulo: "Marcele",
+    descricao:
+      "Marcele é um nome feminino que é uma variação de Marcela, de origem latina, que significa 'pequena guerreira'. É um nome que combina força e delicadeza, frequentemente associado a uma personalidade forte e graciosa.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/marcele/",
+    tags: "marcele Marcele MARCELE",
+  },
+  {
+    titulo: "Gisele",
+    descricao:
+      "Gisele é um nome feminino de origem germânica que significa 'promessa' ou 'refém'. É um nome associado a elegância e sofisticação, frequentemente escolhido por pessoas que apreciam um toque de classe e charme.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/gisele/",
+    tags: "gisele Gisele GISELE",
+  },
+  {
+    titulo: "Alessandra",
+    descricao:
+      "Alessandra é um nome feminino de origem italiana que significa 'protetora da humanidade'. É um nome imponente e elegante, frequentemente associado a pessoas com uma presença forte e carismática.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/alessandra/",
+    tags: "alessandra Alessandra ALESSANDRA",
+  },
+  {
+    titulo: "Bruna",
+    descricao:
+      "Bruna é um nome feminino de origem germânica que significa 'marrom' ou 'escura'. É um nome simples e forte, frequentemente associado a uma personalidade decidida e autêntica.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/bruna/",
+    tags: "bruna Bruna BRUNA",
+  },
+  {
+    titulo: "Priscila",
+    descricao:
+      "Priscila é um nome feminino de origem latina que significa 'antiga' ou 'venerável'. É um nome que carrega um senso de tradição e dignidade, frequentemente associado a pessoas respeitáveis e confiáveis.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/priscila/",
+    tags: "priscila Priscila PRISCILA",
+  },
+  {
+    titulo: "Fernanda",
+    descricao:
+      "Fernanda é um nome feminino de origem germânica que significa 'ousada na paz'. É um nome que transmite uma sensação de força e calma, frequentemente associado a pessoas equilibradas e determinadas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/fernanda/",
+    tags: "fernanda Fernanda FERNANDA",
+  },
+  {
+    titulo: "Vanessa",
+    descricao:
+      "Vanessa é um nome feminino de origem grega que pode significar 'borboleta'. É um nome que transmite leveza e graça, frequentemente associado a pessoas encantadoras e de espírito livre.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/vanessa/",
+    tags: "vanessa Vanessa VANESSA",
+  },
+  {
+    titulo: "Vitoria",
+    descricao:
+      "Vitoria é uma variação de Vitória, de origem latina, que significa 'vitória'. É um nome associado a conquistas e sucesso, frequentemente escolhido por pessoas que têm uma forte determinação e ambição.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/vitoria/",
+    tags: "vitoria Vitoria VITORIA",
+  },
+  {
+    titulo: "Gaia",
+    descricao:
+      "Gaia é um nome feminino de origem grega que significa 'terra' ou 'mãe terra'. Na mitologia grega, Gaia é a deusa primordial da Terra, simbolizando a fertilidade e a abundância. É um nome associado à natureza e à harmonia com o mundo natural.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/gaia/",
+    tags: "gaia Gaia GAIA",
+  },
+  {
+    titulo: "Gabriela",
+    descricao:
+      "Gabriela é um nome feminino de origem hebraica que significa 'Deus é a minha força'. É uma forma feminina de Gabriel, muito popular em diversos países. Gabriela é frequentemente associada a pessoas gentis e decididas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/gabriela/",
+    tags: "gabriela Gabriela GABRIELA",
+  },
+  {
+    titulo: "Amanda",
+    descricao:
+      "Amanda é um nome feminino de origem latina que significa 'digna de amor' ou 'amada'. É um nome clássico e elegante, associado a pessoas afetuosas e carinhosas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/amanda/",
+    tags: "amanda Amanda AMANDA",
+  },
+  {
+    titulo: "Carolina",
+    descricao:
+      "Carolina é um nome feminino de origem latina que significa 'pequena mulher forte'. É um nome que transmite uma sensação de força e graça, frequentemente associado a pessoas determinadas e gentis.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/carolina/",
+    tags: "carolina Carolina CAROLINA",
+  },
+  {
+    titulo: "Daniela",
+    descricao:
+      "Daniela é um nome feminino de origem hebraica que significa 'Deus é meu juiz'. É a forma feminina de Daniel e é popular em diversos países. Daniela é frequentemente associada a pessoas inteligentes e sensíveis.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/daniela/",
+    tags: "daniela Daniela DANIELA",
+  },
+  {
+    titulo: "Eduarda",
+    descricao:
+      "Eduarda é um nome feminino de origem germânica que significa 'guardiana das riquezas'. É um nome que transmite uma sensação de elegância e dignidade, frequentemente associado a pessoas nobres e decididas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/eduarda/",
+    tags: "eduarda Eduarda EDUARDA",
+  },
+  {
+    titulo: "Joana",
+    descricao:
+      "Joana é um nome feminino de origem hebraica que significa 'Deus é gracioso'. É uma forma feminina de João e é popular em várias culturas. Joana é frequentemente associada a pessoas gentis e confiáveis.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/joana/",
+    tags: "joana Joana JOANA",
+  },
+  {
+    titulo: "Ilana",
+    descricao:
+      "Ilana é um nome feminino de origem hebraica que significa 'árvore' ou 'linda'. É um nome que carrega uma conotação de natureza e beleza, frequentemente associado a uma personalidade suave e elegante.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/ilana/",
+    tags: "ilana Ilana ILANA",
+  },
+  {
+    titulo: "Natália",
+    descricao:
+      "Natália é um nome feminino de origem latina que significa 'natal' ou 'dia do nascimento'. É um nome que transmite uma sensação de festividade e celebração, frequentemente associado a pessoas vibrantes e calorosas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/natalia/",
+    tags: "natalia Natália NATALIA",
+  },
+  {
+    titulo: "Pamela",
+    descricao:
+      "Pamela é um nome feminino de origem grega que significa 'toda a doçura'. É um nome associado a pessoas encantadoras e amáveis, com um toque de sofisticação e elegância.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/pamela/",
+    tags: "pamela Pamela PAMELA",
+  },
+  {
+    titulo: "Patrícia",
+    descricao:
+      "Patrícia é um nome feminino de origem latina que significa 'nobre'. É um nome tradicional e sofisticado, frequentemente associado a pessoas com uma presença marcante e respeitável.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/patricia/",
+    tags: "patricia Patrícia PATRÍCIA",
+  },
+  {
+    titulo: "Ronara",
+    descricao:
+      "Ronara é um nome feminino que não possui uma origem amplamente conhecida, mas pode ser visto como uma variação moderna e única. É um nome que transmite uma sensação de originalidade e singularidade.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/ronara/",
+    tags: "ronara Ronara RONARA",
+  },
+  {
+    titulo: "Rebeca",
+    descricao:
+      "Rebeca é um nome feminino de origem hebraica que significa 'ligação' ou 'prisão'. É um nome clássico e elegante, frequentemente associado a pessoas sensíveis e dedicadas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/rebeca/",
+    tags: "rebeca Rebeca REBECA",
+  },
+  {
+    titulo: "Samanta",
+    descricao:
+      "Samanta é um nome feminino de origem aramaica que significa 'ouvir'. É uma variação moderna de Samantha e é frequentemente associado a pessoas atenciosas e compreensivas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/samanta/",
+    tags: "samanta Samanta SAMANTA",
+  },
+  {
+    titulo: "Samara",
+    descricao:
+      "Samara é um nome feminino de origem aramaica que significa 'protetora'. É um nome que transmite uma sensação de segurança e cuidado, frequentemente associado a pessoas generosas e atenciosas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/samara/",
+    tags: "samara Samara SAMARA",
+  },
+  {
+    titulo: "Simone",
+    descricao:
+      "Simone é um nome feminino de origem francesa que é uma variação de Simon, significando 'ouvir'. É um nome sofisticado e elegante, frequentemente associado a pessoas perspicazes e empáticas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/simone/",
+    tags: "simone Simone SIMONE",
+  },
+  {
+    titulo: "Silvana",
+    descricao:
+      "Silvana é um nome feminino de origem latina que significa 'da floresta'. É um nome que transmite uma conexão com a natureza e uma sensação de tranquilidade e frescor.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/silvana/",
+    tags: "silvana Silvana SILVANA",
+  },
+  {
+    titulo: "Solange",
+    descricao:
+      "Solange é um nome feminino de origem francesa que significa 'solene'. É um nome que transmite uma sensação de dignidade e elegância, frequentemente associado a pessoas de caráter forte e nobre.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/solange/",
+    tags: "solange Solange SOLANGE",
+  },
+  {
+    titulo: "Tatiana",
+    descricao:
+      "Tatiana é um nome feminino de origem russa que significa 'pertencente à família Tatius'. É um nome que transmite uma sensação de sofisticação e nobreza, frequentemente associado a pessoas elegantes e de caráter forte.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/tatiana/",
+    tags: "tatiana Tatiana TATIANA",
+  },
+  {
+    titulo: "Tamires",
+    descricao:
+      "Tamires é um nome feminino de origem tupi-guarani que pode significar 'corpo de água'. É um nome que transmite uma conexão com a natureza e a serenidade, frequentemente associado a pessoas tranquilas e equilibradas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/tamires/",
+    tags: "tamires Tamires TAMIRE",
+  },
+  {
+    titulo: "Zuleica",
+    descricao:
+      "Zuleica é um nome feminino de origem árabe que significa 'bela' ou 'linda'. É um nome raro e exótico, frequentemente associado a pessoas sofisticadas e elegantes.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/zuleica/",
+    tags: "zuleica Zuleica ZULEICA",
+  },
+  {
+    titulo: "Zurema",
+    descricao:
+      "Zurema é um nome feminino de origem indígena que pode significar 'luz' ou 'claridade'. É um nome raro e distintivo, frequentemente associado a pessoas com uma presença marcante e iluminadora.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/zurema/",
+    tags: "zurema Zurema ZUREMA",
+  },
+  {
+    titulo: "Eulália",
+    descricao:
+      "Eulália é um nome feminino de origem grega que significa 'bem falada' ou 'eloquente'. É um nome sofisticado e de caráter nobre, frequentemente associado a pessoas com habilidades comunicativas e um espírito vibrante.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/eulalia/",
+    tags: "eulalia Eulália EULÁLIA",
+  },
+  {
+    titulo: "Marieta",
+    descricao:
+      "Marieta é um nome feminino que é uma variação de Maria, e pode significar 'amada' ou 'senhora'. É um nome clássico e carinhoso, frequentemente associado a pessoas gentis e de grande coração.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/marieta/",
+    tags: "marieta Marieta MARIETA",
+  },
+  {
+    titulo: "Maria",
+    descricao:
+      "Maria é um nome feminino de origem hebraica que significa 'senhora' ou 'amada'. É um dos nomes mais tradicionais e universais, associado a pessoas com uma presença forte e significativa.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/maria/",
+    tags: "maria Maria MARIA",
+  },
+  {
+    titulo: "Jessica",
+    descricao:
+      "Jessica é um nome feminino de origem hebraica que significa 'visível' ou 'rica'. É um nome popular e moderno, frequentemente associado a pessoas inteligentes e carismáticas.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/jessica/",
+    tags: "jessica Jessica JESSICA",
+  },
+  {
+    titulo: "Fatima",
+    descricao:
+      "Fatima é um nome feminino de origem árabe que significa 'a que desmama'. É um nome tradicional e reverenciado, frequentemente associado a pessoas com uma presença calma e protetora.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/fatima/",
+    tags: "fatima Fatima FATIMA",
+  },
+  {
+    titulo: "Larissa",
+    descricao:
+      "Larissa é um nome feminino de origem grega que significa 'cidade do mar'. É um nome suave e melodioso, frequentemente associado a pessoas com uma personalidade tranquila e elegante.",
+    link: "https://revistacrescer.globo.com/guia-de-nomes/larissa/",
+    tags: "larissa Larissa LARISSA",
+  },
+];
